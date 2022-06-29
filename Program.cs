d1925c4a7050c7f2235661611f048cdabb8d4703
Varia S <especificita@gmail.com>
2022-06-29 15:18:43 +0300

    Task 9 Method to generate random number and print its largest digit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка 10-99 и показывает наибольшую цифру числа.
+
+Random rand = new Random();// Random - тип данных
+int number = rand.Next(10,100);
+Task9(number);
+void Task9(int currentNumber)
+{
+    
+    Console.WriteLine(currentNumber);
+    int digitFirst = currentNumber/10;
+    int digitSecond = currentNumber%10;
+    if (digitFirst > digitSecond)
+    {
+        Console.WriteLine(digitFirst);
+    }
+    else
+    {
+        Console.WriteLine(digitSecond);
+    }
+}
