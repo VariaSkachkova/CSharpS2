731816fe3d2080d10d7897e180f9f5b15bb03a8f
Varia S <especificita@gmail.com>
2022-07-01 11:49:14 +0300

    Added Task 14 and Task 16

--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@ void Task11Alt (string number)
 на вход два числа и выводить, является ли второе
 число кратным первому. Если число 2 не кратно числу
 1, то программа выводит остаток от деления.*/
-Console.WriteLine("Введите первое целое число: ");
+/*Console.WriteLine("Введите первое целое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -68,5 +68,44 @@ void Task12 (int num1, int num2)
     else
     {
         Console.WriteLine(result);
+    }
+}*/
+// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+
+/*Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Task14(n);
+void Task14 (int n)
+{
+    if (n % 7 == 0 && n % 23 == 0) // && - краткая конъюнкция (смотрит истинность первого значения и если ложное, дальше не проверяет), & - обычная конъюнкция
+    {
+        Console.WriteLine("Это число кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Это число не кратно 7 и 23");
+    }
+}*/
+
+// Напишите программу, которая принимает на
+//вход два числа и проверяет, является ли одно
+//число квадратом другого.
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Task16(num1, num2);
+void Task16 (int num1, int num2) 
+{
+    if (num2 == num1 * num1 | num1 == num2 * num2)
+    {
+        Console.WriteLine("Одно из чисел является квадратом другого");
+    }
+    else
+    {
+        Console.WriteLine("Ни одно из чисел не является квадратом другого");
     }
 }
